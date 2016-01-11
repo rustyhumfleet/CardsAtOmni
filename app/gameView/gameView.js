d3c4dfbf537f6dbdf6d98d24243b5ba0e9fbd542
Rusty Humfleet <rustyhumfleet@yahoo.com>
2016-01-10 23:24:01 -0500

    Fixed code because init commit fried

---
+++ app/gameView/gameView.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.gameView', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/gameView', {
+    templateUrl: 'gameView/gameView.html',
+    controller: 'gameViewCtrl'
+  });
+}])
+
+.controller('gameViewCtrl', [function() {
+
+}]);
---
+++ app/gameView/gameView_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.gameView module', function() {
+
+  beforeEach(module('myApp.gameView'));
+
+  describe('gameView controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var view1Ctrl = $controller('gameViewCtrl');
+      expect(view1Ctrl).toBeDefined();
+    }));
+
+  });
+});
