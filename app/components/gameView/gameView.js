1d2b92bdc1264d4c6b731bd0c2f2977cb38a4291
Rusty Humfleet <rhumfleet@omnispear>
2016-01-17 21:27:38 -0500

    Player can now select card to play area

--- app/components/gameView/gameView.js
+++ app/components/gameView/gameView.js
@@ -2,21 +2,43 @@
 
 angular
 	.module('myApp.gameView', ['ngRoute'])
-	.controller('gameViewCtrl', [function($scope) {
-//controller logic here
-	//$scope.message = 'This is the gameViewCtrl';
+	.controller('gameViewCtrl', ['$scope', function($scope) {
+	$scope.deckOfCards = buildDeck();
+	$scope.selectedCard = "Please Select A Card";
+
+	$scope.playerCardClicked = function(id) {		
+		$scope.deckOfCards[id].selected = !$scope.deckOfCards[id].selected;
+		if($scope.deckOfCards[id].selected){
+			$scope.cardMessage = $scope.deckOfCards[id].message;
+			$scope.selectedCard = $scope.cardMessage;
+		}		
+	};	
+
+	$scope.reset = function () {
+		$scope.deckOfCards.forEach(function(card) {
+	    	card.selected = false;
+    	});
+    	$scope.selectedCard = "Please Select A Card";
+    };
 }]);
 
-/*
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/gameView', {
-    templateUrl: 'components/gameView/gameView.html',
-    controller: 'gameViewCtrl'
-  });
-}])
-a*/
+function buildDeck() {
+	 var deck = [
+	 	"dudes dudes dudes",
+	 	"whines like a little barking dog",
+	 	"drives really slow",
+	 	"trololos",
+	 	"gays"
+	 ];
 
-
-function playerCardClicked() {
-	
+	var deckReady = [];
+	for (var x = 0; x <= 4; x++) {
+		 var card = {
+		 	id: x,
+	 		selected: false,
+	 		message: deck[x]
+	 	};
+	 	deckReady.push(card);
+	};
+	return deckReady;
 };
