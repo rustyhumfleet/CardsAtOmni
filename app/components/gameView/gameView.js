94c3c7d440fafd9f0c444a30ab33fd7c86389252
Rusty Humfleet <rhumfleet@omnispear>
2016-02-12 17:54:20 -0500

    updated some stuff with T Reed

--- app/components/gameView/gameView.js
+++ app/components/gameView/gameView.js
@@ -4,23 +4,46 @@ angular
 	.module('myApp.gameView', ['ngRoute'])
 	.controller('gameViewCtrl', ['$scope', function($scope) {
 	$scope.deckOfCards = buildDeck();
-	$scope.selectedCard = "Please Select A Card";
+	$scope.selectedCard = buildCard(0, true, "Please Select A Card");
+	$scope.customCard = buildCard(0, false, "Fill in the text for a custom card.");
 
 	$scope.playerCardClicked = function(id) {		
 		$scope.deckOfCards[id].selected = !$scope.deckOfCards[id].selected;
 		if($scope.deckOfCards[id].selected){
 			$scope.cardMessage = $scope.deckOfCards[id].message;
-			$scope.selectedCard = $scope.cardMessage;
+			$scope.selectedCard.message = $scope.cardMessage;
 		}		
 	};	
+
+	$scope.setMessage = function(card) {
+		card.message = "";
+	};
 
 	$scope.reset = function () {
 		$scope.deckOfCards.forEach(function(card) {
 	    	card.selected = false;
     	});
-    	$scope.selectedCard = "Please Select A Card";
+    	$scope.selectedCard = buildCard(0, true, "Please Select A Card");
     };
+
+    $scope.selectCustomCard = function (card) {    	
+    	$scope.customCard.message = card.message;
+    	$scope.customCard.selected = true;
+    	$scope.selectedCard.message = $scope.customCard.message;
+    	$scope.selectedCard.selected = $scope.customCard.selected;
+    };
+
+
 }]);
+
+function buildCard(id, selected, message) {
+	var card = {
+			 	id: id,
+		 		selected: selected,
+		 		message: message
+	 };
+	 return card;
+};
 
 function buildDeck() {
 	 var deck = [
